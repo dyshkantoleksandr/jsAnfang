8ea1e0214416837d4b3d78c4fee428cab8993d3d
Sascha <dyshkant.oleksandr@yahoo.de>
2022-10-16 17:27:47 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+'use strict';
+// let number = 5;
+// console.log(number);
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 'John',
+//     b: 25,
+//     c: 'Potter'
+// };
+
+// console.log(obj[0]);
+
+// const arrObj = {
+//     a: 1,
+//     b: 25,
+//     c: 'Potter'
+// };
+// arrObj.userName = 'Harry';
+// console.log(arrObj['userName']);
+  
+// const result = confirm('Are you here?');
+// console.log(result);
+ 
+const answers = [];
+
+answers[0] = prompt('Wie heist du?', ''),
+answers[1] = prompt('Wie alt bist du?', ''),
+answers[2] = prompt('Wo wohnst du ?', '');
+
+document.write(answers);
+
